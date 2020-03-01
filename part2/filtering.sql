c5b3ef5c9de21ced1ef9b416492d2e3f9eb81244
mechanic <tanviredu@yahoo.com>
2020-03-01 17:32:30 +0600

    added

---
+++ part2/filtering.sql
@@ -0,0 +1,30 @@
+
+-- this is the basic command for the 
+-- for querying any data 
+-- and remember we will add more filter 
+-- with it
+SELECT column_1,column_2 from table;
+
+-- if you want to find something that is equal to something then
+
+SELECT column1,column2 from table where value=<simething>
+
+-- you want to exclude someting then you have to use the not equal cluse which is "<>"
+
+Select column_1,column_2 from table where value <> something
+
+-- greater than
+Select column_1,column_2 from table where value
+    > something
+
+-- less than
+
+Select column_1,column_2 from table where value
+    < something
+
+
+
+-- greater than and equal
+-- less than and equal
+-- BETWEEN a range
+-- ISNULL
