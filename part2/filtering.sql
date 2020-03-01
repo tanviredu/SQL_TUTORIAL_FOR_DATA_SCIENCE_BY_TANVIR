5992eb3fc71bdcfe2ced4681874fc53c003d5b5b
mechanic <tanviredu@yahoo.com>
2020-03-01 17:40:52 +0600

    added

--- part2/filtering.sql
+++ part2/filtering.sql
@@ -9,9 +9,31 @@ SELECT column_1,column_2 from table;
 
 SELECT column1,column2 from table where value=<simething>
 
+example:
+
+SELECT product_namee,product,price from store where product_name="banana";
+
+
+
 -- you want to exclude someting then you have to use the not equal cluse which is "<>"
 
 Select column_1,column_2 from table where value <> something
+
+
+-- example
+
+-- always try to write this code in this format
+
+SELECT product_name
+	,price
+	,date
+	,full_name,
+	,company
+
+FROM product_table
+where product_name <> "banana";
+
+
 
 -- greater than
 Select column_1,column_2 from table where value
@@ -25,6 +47,17 @@ Select column_1,column_2 from table where value
 
 
 -- greater than and equal
+
+SELECT product_name,price,date FROM product where price >=150;
+
+-- it will give everything which as price more than 150
+
+
+
+
 -- less than and equal
+SELECT product_name,price,date,full_name FROM product where price <=150;
+
+
 -- BETWEEN a range
 -- ISNULL
