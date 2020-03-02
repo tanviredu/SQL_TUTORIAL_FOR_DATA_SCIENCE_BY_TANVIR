d5f65577b79832413eb230bc74969319660bbebc
mechanic <tanviredu@yahoo.com>
2020-03-02 18:59:08 +0600

    added

---
+++ part2/3_wildcards.sql
@@ -0,0 +1,32 @@
+-- the use of the wildcards and like operator
+
+-- if you dont know the whole name or the value of the data you can use the wildcaed 
+
+-- wild card is used for partial match
+-- like the begineing of the phrase and the ending 
+-- of the pharase
+
+
+-- like is not a operator but it work like this
+
+-- remembe the wild card cant be used for the nymerical value you can use it with the string value
+
+----   % sign
+
+-- suppose you want to find a pizza 
+
+-- %pizza will return value that have pizza in the lasr
+
+-- pizza% will return value that have pizza in the end
+
+-- %pizza% will return any value that have pizza inside it
+
+-- suppose you want to find the gmail adress
+
+-- %@gmail.com
+
+-- it will return everything have the gmail.com
+
+
+
+
