a73ac74e05d6f55b0d3487e6671cb4fcf43ce500
mechanic <tanviredu@yahoo.com>
2020-03-02 19:09:46 +0600

    added

--- part2/4_sorting_OREDER_BY.sql
+++ part2/4_sorting_OREDER_BY.sql
@@ -22,4 +22,8 @@ ORDER BY charistics
 --2) you can use multiple column
 --3) you can sort by thnig evern you donr select it
 --4) you have to add the ORDER BY clause at the last of the statement
+--5) you can sort the column using even column position
+-- you add the cilumn number after the ORDER By clause like ORDER BY 2,3
+
+-- it will sort the column based on the second and the third column
 
